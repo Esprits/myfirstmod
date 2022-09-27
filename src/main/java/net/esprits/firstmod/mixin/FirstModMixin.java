197689d8d9266e00f5841f41e71214505c6cb48d
Esprits <lmignacca@outlook.com>
2022-09-27 00:24:44 -0400

    Setup Completed
    Fully Setup
    + Mod List
    + Icon Changed

--- src/main/java/net/esprits/firstmod/mixin/FirstModMixin.java
+++ src/main/java/net/esprits/firstmod/mixin/FirstModMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.esprits.firstmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.esprits.firstmod.FirstMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class FirstModMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		FirstMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
